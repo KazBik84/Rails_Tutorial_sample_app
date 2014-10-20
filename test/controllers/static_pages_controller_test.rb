166c0817bc56289bccbf4db6dffb8ae6f08c5189
kask666 <kazimierz.bik@gmail.com>
2014-10-20 08:43:30 +0000

    20.10.2014 Doszedlem do pierwszych testow, listing 3.11

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
+    # symuluje żądanie home
     get :home
+    # zakłada odpowiedź servera 200 czyli sukces 
     assert_response :success
   end
 
