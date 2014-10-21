dd45ff275475a78dc31a077b0fde0d368060699f
kask666 <kazimierz.bik@gmail.com>
2014-10-21 08:26:48 +0000

    21.10.2014 Finished Static Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,11 +6,20 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :home
     # zakłada odpowiedź servera 200 czyli sukces 
     assert_response :success
+    # assert_select szuka tagu "title" o zawartości "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"    
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"    
   end
 
 end
