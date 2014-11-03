da45dcf2869cd18cc7f2dc9d3ce545e59b2f1093
kask666 <kazimierz.bik@gmail.com>
2014-11-03 13:41:49 +0000

    Zakonczylem Advanced test setup i zaczalem 4 rodzial. 03.11.2014

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionController::TestCase
     # zakłada odpowiedź servera 200 czyli sukces 
     assert_response :success
     # assert_select szuka tagu "title" o zawartości "Home | Ruby on Rails Tutorial Sample App"
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
